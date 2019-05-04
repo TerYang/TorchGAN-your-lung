d75e07221ab1aa6f5dc2a8ff8cfc1634c4d49236
TerYang <adau22@163.com>
2019-05-04 14:58:59 +0800

    add drawfunc.py

---
+++ draw_func.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+# @Time   : 19-4-24 下午9:45
+# @Author : TerYang
+# @contact : adau22@163.com ============================
+# my github:https://github.com/TerYang/              ===
+# all rights reserved                                ===
+# good good study,day day up!!                       ===
+# ======================================================
+import pandas as pd
+import seaborn as sb
+
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# D_PATH = '/home/gjj/PycharmProjects/ADA/TorchGAN-your-lung/test_new/draw'
+D_PATH = '/home/gjj/PycharmProjects/ADA/TorchGAN-your-lung/tests_new_data'
+thedataset = 'tests_new_data'
+result_url = '/home/gjj/PycharmProjects/ADA/TorchGAN-your-lung/draw_pictures'
+
+def draw_func(url,color=None):
+    """
+    func: draw
+    :param url:
+    :return:
+    "-" (solid) – default
+    "--" (dashed)
+    "-." (dash dot)
+    ":" (dotted)
+    "None" or " " or "" (nothing)
+        'b'	blue
+    'g'	green
+    'r'	red
+    'c'	cyan
+    'm'	magenta
+    'y'	yellow
+    'k'	black
+    'w'	white
+    """
+    df = pd.read_csv(url, engine='python',names=['p','n','a'])
+    df = df.dropna(axis=1,how='all')
+
+    # line = 'solid'
+    for n in df.columns:
+        if n == 'n':
+            line = 'dash dot'
+            # color = 'm'
+        elif n == 'a':
+            line = 'dotted'
+            # color = 'b'
+        else:
+            line = 'solid'
+            # color = 'r'
+        p = pd.Series(df[n])
+        p = p.dropna(axis=0, how=any)
+        try:
+            plt.plot(p, label=n, color=color, linestyle=line, marker='x')  # 'x'
+            # plt.annotate("(%s,%s)" % p, xy=p, xytext=(-20, 10), textcoords='offset points')
+            plt.xlabel('Modules:No.')
+            plt.ylabel('rate')
+
+        except:
+            print(p)
+            # print('________________________')
+            pass
+
+
+if __name__ == '__main__':
+
+    # import seaborn as sns
+    #
+    # sns.set()
+    # import matplotlib.pyplot as plt
+    # fmri = sns.load_dataset("fmri")
+    # print(type(fmri))
+    # print(fmri)
+    # ax = sns.lineplot(x=fmri.index, y=fmri['signal'], data=fmri)#"timepoint","signal"
+    # plt.show()
+    # exit()
+    urls = []
+    dires = os.listdir(D_PATH)#modules names
+    dires = sorted(dires)
+    for root, dirs, files in os.walk(D_PATH, topdown=None):
+        for name in dirs:
+            if name == 'collection':
+                urls.append('{}'.format(os.path.join(root,name)))
+    file = [os.path.join(url,file_n) for url in urls for file_n in os.listdir(url) if '_collection.csv' in file_n]
+    file = sorted(file)
+    # print(file)
+    # print(dires)
+    # exit()
+
+    # colors = ['g','r','b','k']
+
+    # lights = [.1,.3,.5,.7,.9]
+    # current_palette = sns.color_palette()
+    # print(type(current_palette),len(current_palette))
+    # columns = ['P','N','PF','NF','accurate','recall','F1']
+    # sns.palplot(sns.hls_palette(8, l=.3, s=.8))
+    sns.set()
+    path_ = os.path.join(result_url,thedataset)
+    if not os.path.exists(path_):
+        os.makedirs(path_)
+    for filefolder in dires:#tests　目錄下，每一個模型
+        for f in file:
+            if filefolder in f:
+                t = plt.figure()
+                path = os.path.join(path_, filefolder + '/'+os.path.splitext(os.path.basename(f))[0]+ '_loss.png')
+                if not os.path.exists(os.path.join(path_, filefolder)):
+                    os.makedirs(os.path.join(path_, filefolder))
+                df = pd.read_csv(f, engine='python',header=None,dtype=np.str)
+                # print(df)
+                columns = df.iloc[0,:].values.tolist()[1:]
+                # print(columns)
+                # df = pd.read_csv(f, engine='python',names=columns,header=None)
+                df = df.iloc[1:,:]
+                indexs_ = df[0].values.tolist()
+                # print(indexs_)
+                df = df.iloc[:,1:].values.astype(np.float64)
+                # print(df)
+                df = pd.DataFrame(df,index=indexs_,columns=columns,dtype=np.float64)#
+                # print(df)
+                df = df.dropna(axis=1,how='all')
+                df = df.dropna(axis=0,how='any')
+                # sns.set(style="whitegrid")
+                # print(df)
+                # print(df.columns)
+                n = len(df.columns)
+                # print(df.columns)
+                # print(df)
+                # palette = sns.cubehelix_palette(light=light, n_colors=n)
+                # ax = sns.lineplot(x=df.index[:-1], y=df['N'], data=df[:-1,:])  # "timepoint","signal"
+
+                # sns.lineplot(data=df, palette="tab10", linewidth=2.5)
+
+                # ax = sns.lineplot(x=df.index, y=df[df.columns], data=df,palette="tab10", linewidth=2.5)#"timepoint","signal"
+
+                df.plot()#ok ok ok ok ok ok
+
+                # print(df)
+                # exit()
+                # sns.lineplot(data=df, palette="tab10", linewidth=2.5)
+                # sns.lineplot(data=df, palette=palette, linewidth=2.5,hue="coherence", style="choice",)
+                plt.legend(loc=4)
+                plt.savefig(path)
+                plt.clf()
+                # plt.cla()
+                plt.close('all')
+                # plt.tight_layout()
+                # plt.show()
